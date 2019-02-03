3429b2d1efd939e322a11e3de62c666fd8aad865
lukeludlow <lukeludlow@live.com>
2019-02-03 16:49:21 -0700

    database access stuff

--- src/dao/AuthTokenDao.java
+++ src/dao/AuthTokenDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import model.AuthToken;
+import java.sql.*;
+
+/**
+ * authtoken data access object
+ * used to connect to database and do stuff with model objects
+ */
 public class AuthTokenDao {
+    /** default constructor */
+    public AuthTokenDao() {
+    }
+    /**
+     * generate new token with random int value
+     * also inserts it into database
+     * @param username user (username) to which this token belongs to
+     * @return new AuthToken
+     */
+    public AuthToken generateAuthToken(String username) {
+        return new AuthToken();
+    }
 }
--- src/dao/PersonDao.java
+++ src/dao/PersonDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import model.AuthToken;
+import model.Person;
+
+import java.sql.*;
+
+/**
+ * person data access object
+ * used to connect to database and do stuff with model objects
+ */
 public class PersonDao {
+
+
+
+
+
+    public Person generatePerson() {
+        return new Person();
+    }
+
 }
