e96511b80c7949c0c10117bf63c2ba3caf700d7f
lukeludlow <lukeludlow@live.com>
2019-02-04 10:33:08 -0700

    pretty sql

---
+++ src/fmserver/Server.java
@@ -0,0 +1,13 @@
+package fmserver;
+
+/**
+ * family map server
+ *
+ * this is the main server class
+ * @author luke ludlow
+ */
+public class Server {
+    public static void main (String[] args) {
+
+    }
+}
