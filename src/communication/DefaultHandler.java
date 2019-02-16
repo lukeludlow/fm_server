40686b29c98ee25e6f34a880c4216b02765c450b
lukeludlow <lukeludlow@live.com>
2019-02-16 14:45:49 -0700

    server and default web handler

--- src/communication/DefaultHandler.java
+++ src/communication/DefaultHandler.java
@@ -1,4 +1,39 @@
 package communication;
 
-public class DefaultHandler {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DefaultHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        boolean success = false;
+        try {
+            URI uri = exchange.getRequestURI();
+            String pathName = Paths.get("").toAbsolutePath().toString() + "/web" + uri.getPath();
+            if (uri.getPath().equals("/")) {
+                pathName += "index.html";
+            }
+            System.out.println("web file path: " + pathName);
+            Path path = FileSystems.getDefault().getPath(pathName);
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+            Files.copy(path, exchange.getResponseBody());
+            exchange.getResponseBody().close();
+        } catch (IOException ex) {
+            // internal server error
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
+            exchange.getResponseBody().close();
+            System.err.println(ex);
+        }
+    }
+
+
 }
--- src/fmserver/Server.java
+++ src/fmserver/Server.java
@@ -1,7 +1,43 @@
 package fmserver;
 
-public class Server {
-    public static void main(String[] args) {
+import com.sun.net.httpserver.HttpServer;
+import communication.ClearHandler;
+import communication.DefaultHandler;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+
+    private HttpServer server;
+    private static final int MAX_WAITING_CONNECTIONS = 12;
+
+    private void run(String portNumber) {
+        System.out.println("initializing http server");
+        try {
+            server = HttpServer.create(new InetSocketAddress(Integer.parseInt(portNumber)), MAX_WAITING_CONNECTIONS);
+        } catch (IOException ex) {
+            System.err.println(ex.toString());
+            return;
+        }
+        server.setExecutor(null);
+        System.out.println("creating contexts");
+
+
+
+        server.createContext("/clear", new ClearHandler());
+        server.createContext("/", new DefaultHandler());
+
+
+
+        System.out.println("starting server");
+        server.start();
+        System.out.println("success");
     }
+
+    public static void main(String[] args) {
+        String port = args[0];
+        new Server().run(port);
+    }
+
 }
